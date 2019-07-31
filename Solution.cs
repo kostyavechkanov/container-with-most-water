7adf7cd8c187e6518d1ec12bdb851b377875a154
Kostiantyn Vechkanov <kostyavechkanov3@gmail.com>
2019-07-31 12:41:26 +0300

    Container With Most Water Solution

---
+++ Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ContainerWithMostWater
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int leftI = 0;
+            int rightI = height.Length - 1;
+
+            int maxSize = -1;
+
+            while (rightI > leftI)
+            {
+                int left = height[leftI];
+                int right = height[rightI];
+
+                int size = Math.Min(left, right) * (rightI - leftI);
+                maxSize = Math.Max(size, maxSize);
+
+                if (left < right)
+                    leftI++;
+                else
+                    rightI--;
+            }
+
+            return maxSize;
+        }
+    }
+}
